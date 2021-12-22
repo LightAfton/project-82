4b6579caeab2edc9210634ce2d207e5e3169a605
LightAfton <89879598+LightAfton@users.noreply.github.com>
2021-12-22 17:53:09 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+canvas=document.getElementById("mycanvas");
+ctx=canvas.getContext("2d");
+var mousevent= "empty";
+var last_position_of_x,last_position_of_y;
+canvas.addEventListener("mouseup",my_mouseup);
+function my_mouseup(e)
+{
+mousevent="mouseup";
+}
+canvas.addEventListener("mousedown",my_mousedown);
+function my_mousedown(e)
+{
+mousevent="mousedown";
+}
+canvas.addEventListener("mouseleave",my_mouseleave);
+function my_mouseleave(e)
+{
+mousevent="mouseleave";
+}
+canvas.addEventListener("mousemove",my_mousemove);
+function my_mousemove(e)
+{
+current_position_of_mouse_x=e.clientX-canvas.offsetLeft;
+current_position_of_mouse_y=e.clientY-canvas.offsetTop;
+if(mousevent="mousedown")
+{
+    ctx.beginPath();
+    ctx.strokeStyle="red";
+    ctx.lineWidth=1;
+    ctx.arc(current_position_of_mouse_x,current_position_of_mouse_y,30,0,2*Math.PI);
+    ctx.stroke();
+}
+last_position_of_x=current_position_of_mouse_x;
+last_position_of_y=current_position_of_mouse_y;
+}
